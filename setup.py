e01c84fd944a825b4710db47e96e14eeb0fc8c5a
Sufi Aurangzeb Hossain <azhossain.98.ah@gmail.com>
2025-11-02 23:49:51 +0600

    Add setup.py to create the python_cp_template module

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(
+    name="python_cp_template",
+    version="0.1",
+    py_modules=["python_cp_template"],  # This points to the python_cp_template.py file
+    install_requires=[],                # We can add dependencies here if needed
+)
